7386612956eb506762a10bcb78cf488901f2fe7c
giovani <giovanimouradev@gmail.com>
2025-04-24 10:54:13 -0300

    attribuindo  motivo contato para coluna motivo_contato_id

--- database/migrations/2025_04_24_133944_alter_table_site_contatos_add_fk_motivo_contatos.php
+++ database/migrations/2025_04_24_133944_alter_table_site_contatos_add_fk_motivo_contatos.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class AlterTableSiteContatosAddFkMotivoContatos extends Migration
 {
@@ -13,7 +14,15 @@ class AlterTableSiteContatosAddFkMotivoContatos extends Migration
      */
     public function up()
     {
-        //
+        //adicionando a coluna motivo_contato_id
+        Schema::table('site_contatos',function(Blueprint $table){
+            $table->unsignedBigInteger('motivo_contatos_id');
+
+
+        });
+        //atribuindo motivo_contato para nova coluna motivo_contato_id
+
+        DB::statement('update site_contatos set motivo_contatos_id = motivo_contato');
     }
 
     /**
