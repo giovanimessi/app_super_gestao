4f8b875d2d9d08c6b482c2436e1bbfcac120d965
giovani <giovanimouradev@gmail.com>
2025-05-05 08:27:03 -0300

    Middlware da rota

--- routes/web.php
+++ routes/web.php
@@ -24,61 +24,29 @@ Route::get('/', function () {
     return "Ol·,seja bem vindo ao curso";
 });*/
 
+Route::get('/', 'PrincipalController@principal')->name('site.index')->middleware('log.acesso');
 
-Route::get('/', 'PrincipalController@principal')
-    ->name('site.index');
-Route::get('/sobrenos','SobreNosController@sobrenos')
-->name("site.sobrenos");
-Route::get('/contatos', 'ContatoController@contatos')
-->name('site.contatos');
-Route::post('/contatos', 'ContatoController@create')
-->name('site.create');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contatos')->name('site.contatos');
+Route::post('/contato', 'ContatoController@create')->name('site.contatos');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::get('/login', function () {
-    return "Login";
-})->name('site.login');
+Route::prefix('/app')->group(function() {
+    Route::middleware('autenticacao')
+            ->get('/clientes', function(){return 'Clientes';})
+            ->name('app.clientes');
 
-//agrupar
-Route::middleware(['log.acesso','autenticacao'])->prefix('/app')->group(function () {
+    Route::middleware('autenticacao')
+            ->get('/fornecedores', 'FornecedorContoller@index')
+            ->name('app.fornecedores');
 
-
-    Route::get('/clientes', function () {
-        return "clientes";
-    })->name('app.clientes');
-
-    Route::get('/fornecedores','FornecedorContoller@index')->name('fornecedor');
-    Route::get('/produtos', function () {
-        return "produtos";
-    })->name('app.produtos');
+    Route::middleware('autenticacao')
+            ->get('/produtos', function(){return 'produtos';})
+            ->name('app.produtos');
 });
 
-Route::get('/teste/{p1}/{p2}','TesteController@teste')->name('teste');
+Route::get('/teste/{p1}/{p2}', 'TesteController@teste')->name('site.teste');
 
-Route::fallback(function () {
-    $url = route('site.index');
-    echo "A rota acessada n„o existe. <a href='$url'>Clique aqui</a>";
+Route::fallback(function() {
+    echo 'A rota acessada n√£o existe. <a href="'.route('site.index').'">clique aqui</a> para ir para p√°gina inicial';
 });
-
-/*
-verbo http
-
-get 
-post 
-put 
-patch 
-delete 
-options
-*/
-
-Route::get(
-    '/contato/{nome}/{categoria_id}',
-    function (
-        string $nome = 'Desconhecido',
-        int $categoria_id = 1 //1-informacao
-    ) {
-        echo "Estamos aqui: $nome - $categoria_id";
-    }
-
-)
-    ->where('categoria_id', '[0-9]+')
-    ->where('nome', '[A-Za-z]+');
